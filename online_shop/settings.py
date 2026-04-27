8939fb9402aa03d30345d05a39b1770cc3940cc9
santhoshkumarv09042005 <santhoshkumarv09042005@gmail.com>
2026-04-27 22:01:23 +0530

    fix settings for render deploy 3

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'online_shop.wsgi.application'
 # --------------------------------------------------
 # Database
 # --------------------------------------------------
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
