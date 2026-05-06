69b46511574ce357a3e1bed5fb2c714a37231283
santhoshkumarv09042005 <santhoshkumarv09042005@gmail.com>
2026-05-06 14:26:05 +0530

    add pywebpush to requirements 2

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -25,6 +25,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1 online-shop-w8q3.onrender.com').split()
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # --------------------------------------------------
 # Application Definition
